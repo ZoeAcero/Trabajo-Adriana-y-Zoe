15213c19671d34d4e4369cb7ad37ad4620ecc1af
Francisco <146821904+fferrlop@users.noreply.github.com>
2023-11-06 09:13:45 +0100

    Add files via upload

---
+++ 6_excepciones/main.cpp
@@ -0,0 +1,14 @@
+#include <stdexcept>
+
+void lanzaException() {
+    throw std::runtime_error("Error en la función lanzaException");
+}
+
+
+int main() {
+    lanzaException();
+
+    return 0;
+}
+
+
